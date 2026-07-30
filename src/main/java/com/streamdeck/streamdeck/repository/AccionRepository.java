022be1a491db6259415dd6f0c3f0c036807c556b
blaze43x <blaze43x@gmail.com>
2026-07-29 22:19:37 -0500

    Eliminar todo lo relacionado con accion, esta entidad no existe

--- src/main/java/com/streamdeck/streamdeck/repository/AccionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.streamdeck.streamdeck.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.streamdeck.streamdeck.model.Accion;
-
-public interface AccionRepository extends JpaRepository<Accion, Integer> {
-}
